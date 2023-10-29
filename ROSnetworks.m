25c7e0720bb1983602a724f355e632594f269f90
christianbrischetto <105157612+christianbrischetto@users.noreply.github.com>
2023-10-30 01:18:25 +1100

    final matlab code for object detection

--- ROSnetworks.m
+++ ROSnetworks.m
@@ -1,38 +1,30 @@
-% load using gazebo
 close all
 
-rosshutdown;
+% rosinit if you havent already
 
-% Initialise the ROS node
-rosinit('NodeName', 'ObjectDetection_node')
-
-setenv('ROS_IP','192.168.0.227'); % Jared's IP, also set correct IP on WSL through 
+setenv('ROS_IP','172.25.128.1'); %Christians IP, also set correct IP on WSL through 
 % '$ ifconfig', then set '$ export ROS_IP=xxxx.xxx.xxx.xxx' in WSL window
 
 OBJECT_TO_FIND = 2; % 1 = coke 
                     % 2 = cube
 
 %% Depth Cloud
-fprintf("\nLoading rostopics... \n");
 depthpoints = rossubscriber('/head_camera/depth_registered/points','DataFormat','struct');
 points = receive(depthpoints,1);
 
-fprintf("Extracting point cloud... \n");
 % rosPlot(depth_ros) %makes a pointcloud
 cart = rosReadXYZ(points);
 cartValid = cart(~isnan(cart(:,1)),:);
 
 % orienting in cartesian frame
 holder = cartValid(:,2);
-cartValid(:,2) = cartValid(:,3); %swap x and y
+cartValid(:,2) = cartValid(:,3); %swap y and z
 cartValid(:,3) = holder*-1 + 1.0597; %min value (floor) to set correct origin
 
-%% Camera Tilt Compensation
 fprintf("Compensating for camera tilt... \n");
 tilt_angle = -0.35; % Negative if the camera is tilted down
 Rx = [1 0 0; 0 cos(tilt_angle) -sin(tilt_angle); 0 sin(tilt_angle) cos(tilt_angle)]; % Rotation matrix about x-axis
 cartValid = (Rx * cartValid')'; % Apply rotation
-
 
 % removing unwanted points
 counter = 0;
@@ -40,7 +32,7 @@ cartObject = sortrows(cartValid,3,"ascend"); %sort coloumns by z value
 
 % find values below the table
 for i = 1:numel(cartObject(:,3))
-    if cartObject(i,3) < 0.70 
+    if cartObject(i,3) < 0.70 % for jareds world 
         counter = counter + 1; 
     end
 end
@@ -51,8 +43,8 @@ cartObject(1:counter,:) = [];
 % point clouds
 Objects = pointCloud(cartObject);
 
+
 %% POINT CLOUD OBJECT DETECTION 
-fprintf("Segmenting point cloud... \n");
 % segments point cloud clusters
 [labels, numClusters] = pcsegdist(Objects,0.05);
 
@@ -80,13 +72,6 @@ for i = 1:numClusters
     item{i} = pointCloud(item{i});
 end
 
-switch OBJECT_TO_FIND
-    case 1
-        fprintf("Searching for COKE CAN... \n\n");
-    case 2
-        fprintf("Searching for CUBE... \n\n");
-end
-
 % load in objects to compare to for detection
 switch OBJECT_TO_FIND
     case 1
@@ -105,35 +90,54 @@ features{2} = extractFPFHFeatures(item{2});
 [matchpairs1,scores1] = pcmatchfeatures(objfeatures,features{1},obj,item{1});
 [matchpairs2,scores2] = pcmatchfeatures(objfeatures,features{2},obj,item{2});
 
-
-%                         SOMETHING FUNKY HAPPENS HERE
+%                         something wrong here
 
 if length(matchpairs1) > length(matchpairs2)
     detectedObj = item{1};
+    GraspingPos = GetGraspPos(item{1});
+    % publishing
+    [pub,msg] = rospublisher('position','geometry_msgs/Point');
+    msg.X = GraspingPos(1,1);
+    msg.Y = GraspingPos(1,2);
+    msg.Z = GraspingPos(1,3);
+    send(pub,msg);
+    
+    pause(0.1);
+
+    GraspingPos = GetGraspPos(item{2});
+    % publishing
+    [pub,msg] = rospublisher('position','geometry_msgs/Point');
+    msg.X = GraspingPos(1,1);
+    msg.Y = GraspingPos(1,2);
+    msg.Z = GraspingPos(1,3);
+    send(pub,msg);
+    
 else 
     if length(matchpairs1) < length(matchpairs2)
         detectedObj = item{2};
+        GraspingPos = GetGraspPos(item{2});
+        % publishing
+        [pub,msg] = rospublisher('position','geometry_msgs/Point');
+        msg.X = GraspingPos(1,1);
+        msg.Y = GraspingPos(1,2);
+        msg.Z = GraspingPos(1,3);
+        send(pub,msg);
+        
+        pause(0.1);
+
+        GraspingPos = GetGraspPos(item{1});
+        % publishing
+        [pub,msg] = rospublisher('position','geometry_msgs/Point');
+        msg.X = GraspingPos(1,1);
+        msg.Y = GraspingPos(1,2);
+        msg.Z = GraspingPos(1,3);
+        send(pub,msg);
     else
-        fprintf("!! There has been an error during object detection !! \n");
+        fprintf('There has been an error in object detection');
         return
     end
 end
 
-%% Grasping Position
-xmin = min(detectedObj.Location(:,1));
-xmax = max(detectedObj.Location(:,1));
-ymin = min(detectedObj.Location(:,2));
-ymax = max(detectedObj.Location(:,2));
-zmin = min(detectedObj.Location(:,3));
-zmax = max(detectedObj.Location(:,3));
-xmid = (xmax-xmin)/2;
-ymid = (ymax-ymin)/2;
-
-
-GraspingPos = [(xmin+xmid),(ymin+ymid),(zmax)];
-message = "The Grasping Position is: ";
-disp(message)
-disp(GraspingPos)
 %% Figures
 figure;
 pc2 = pointCloud(cartValid); % view entire scene
@@ -142,11 +146,3 @@ figure;
 pcshow(Objects) % view all objects
 figure;
 pcshow(detectedObj); % view detected object
-
-%% Publishing
-[pub,msg] = rospublisher('position','geometry_msgs/Point');
-
-msg.X = GraspingPos(1,1);
-msg.Y = GraspingPos(1,2);
-msg.Z = GraspingPos(1,3);
-send(pub,msg);
